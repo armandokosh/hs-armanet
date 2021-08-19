dedeec7f0fb34af1837dd956483c944e94454c03
Armando <asifuentes330@gmail.com>
2021-08-19 18:41:25 -0500

    #3 Modelo Article y Migracion

---
+++ database/migrations/2021_08_19_221003_create_routers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoutersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('routers', function (Blueprint $table) {
+            $table->id();
+            $table->string("name")->unique();
+            $table->string("username");
+            $table->string("password");
+            $table->string("dns_name");
+            $table->string('version')->nulleable();
+            $table->string('web_port')->nulleable();
+            $table->string('lan_interface')->nulleable();
+            $table->integer('api_port')->default(8728);
+            $table->string('coordinates')->nulleable();
+            $table->string('status')->default('online');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('routers');
+    }
+}
