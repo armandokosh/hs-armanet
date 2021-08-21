ef241fbc93d483658390d53e1f8a9bdc9227d454
Armando <asifuentes330@gmail.com>
2021-08-21 12:14:21 -0500

    #8 Auth JWT Proteccion de rutas con Middleware

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -29,6 +30,8 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
+        'name',
+        'email',
         'password',
         'remember_token',
     ];
@@ -41,4 +44,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
